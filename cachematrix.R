b33954449d0b2180e364adb4da6981ff16519554
IvyKuo <hsuanyingkuo@gmail.com>
2014-06-21 22:38:41 -0700

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,37 @@
 ## Create a pair of functions that cache the inverse of a matrix
 
 ## This function creates a special matrix object that can cache its inverse 
+## Assume the matrix supplied is always invertible
 ## set the value of matrix 
 ## get the value of matrix
 ## set the value of inverse of the matrix
 ## get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function (y) {
-    x <<- y
-    inv <<- NULL     
-  }
-  get <- function () x
-  setinv <- function (inverse) inv <<- inverse
-  getinv <- function () inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+            inv <- NULL
+            set <- function (y) {
+                  x <<- y
+                  inv <<- NULL     
+            }
+            get <- function () x
+            setinv <- function (inverse) inv <<- inverse
+            getinv <- function () inv
+            list(set = set, get = get,
+                 setinv = setinv,
+                 getinv = getinv)
 }
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-## If the inverse has already been calculated, then the cachesolve should retrieve the inverse from the cache.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if (!is.null(inv)) {
-    message("getting cached data")
-    return(inv)   
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
-  inv
+        inv <- x$getinv()
+        if (!is.null(inv)) {
+          message("getting cached data")
+          return(inv)   
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
 
 ## Test above functions:
